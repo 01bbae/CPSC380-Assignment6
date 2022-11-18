a5976791c7acf18bb8aa10b5aaf94fbea82d2525
BJ <01bbae@gmail.com>
2022-11-18 04:46:10 -0800

    Finished Assignment

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # CPSC380-Assignment6
 
+Name: BJ Bae
 
+Email: bbae@chapman.edu
 
+Assignment: Assignment 6 Virtual Address Manager
+
+Class: CPSC 380-02
+
+References:
 https://stackoverflow.com/questions/3501338/c-read-file-line-by-line
+https://www.programiz.com/dsa/circular-queue
--- vmmgr.c
+++ vmmgr.c
@@ -11,7 +11,7 @@
 #define frameSize 256
 #define pageSize 256
 #define totalFrames 256
-#define numInTLB 16
+#define TLBSize 16
 #define numInPageTable 256
 #define totalPhysMem 65536
 
@@ -47,14 +47,19 @@ int main(int argc, char const *argv[])
     // for statistics
     int pageHits = 0;
     int pageMisses = 0;
+    int rowCount = 0;
+
+    int pageHitpageNo[10][3];
 
     //initialize TLB
-    tlbent* tlb[numInTLB];
-    int tlbstart = 0; // for keeping track of fifo queue start
-    int tlbend = 0; // for keeping track of end of queue to add element
+    tlbent tlb[TLBSize];
+    bool TLBFull = false;
+    int tlbstart = -1; // for keeping track of fifo queue start
+    int tlbend = -1; // for keeping track of end of queue to add element
 
     //initialize and declare physical memory
-    char** physMem[totalFrames];
+    char* physMem[totalFrames];
+    int nextPhysMem = 0;
 
     //initialize backing store
     FILE* bs = fopen("BACKING_STORE.bin", "rb");
@@ -63,11 +68,16 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE);
     }
     char* backingStore = mmap(NULL, totalPhysMem, PROT_READ, MAP_SHARED, fileno(bs), 0);
-    for (int i = 0; i < totalPhysMem; ++i){
-        printf("%x",backingStore[i]);
+    if (backingStore == MAP_FAILED) {
+        perror("mmap");
+        exit(1);
     }
-    printf("\n");
-    printf("%d\n",backingStore[66]);
+    // int count = 0;
+    // for (int i = 0; i < totalPhysMem; ++i){
+    //     printf("%x",backingStore[i]);
+    //     count++;
+    // }
+    // printf("\n%d\n", count);
 
     //initialize page table
    ptent* pt[numInPageTable];
@@ -83,88 +93,93 @@ int main(int argc, char const *argv[])
     while ((read = getline(&line, &len, fp)) != -1)
     {
         uint32_t addr = atoi(line);
-        printf("number: %d\n", addr);
+        // printf("number: %d\n", addr);
+        rowCount++;
 
         char* output;
 
         laddr.ul = addr;
-        printf("laddr bf pageno: %d\n", laddr.bf.pageno);
-        printf("laddr bf offset: %d\n", laddr.bf.offset);
+        // printf("laddr bf pageno: %d\n", laddr.bf.pageno);
+        // printf("laddr bf offset: %d\n", laddr.bf.offset);
         if (laddr.bf.offset > 256){
             perror("Error: offset exceeds page size");
             exit(EXIT_FAILURE);
         }
-	printf("%x\n", backingStore[66]);
         // search tlb for page hit
         uint8_t frameno; // translate pageno to frameno
         uint8_t offset = laddr.bf.offset; // offset is directly translated
         bool inTLB = false;
-        for (int i = 0; i < tlbend; ++i){ // search until the end of tlb for pageno
-            if (tlb[i]->pageno == laddr.bf.pageno){
+        for (int i = tlbstart; i != tlbend; i = (i+1)%TLBSize){ // search until the end of tlb for pageno
+            // printf("%d pageno: %d, frameno: %d\n", i, tlb[i].pageno, tlb[i].frameno);
+            if (tlb[i].pageno == laddr.bf.pageno){
                 inTLB = true;
-                frameno = tlb[i]->frameno;
-                pageHits++;
+                frameno = tlb[i].frameno; //if its in TLB get frameno
+
             }
         }
         if (!inTLB){
-            printf("frameno not in tlb\n");
+            // printf("frameno not in tlb\n");
             pageMisses++;
             // acquire from page table
             if (!pt[laddr.bf.pageno]->valid){
-                printf("FRAME DOES NOT EXIST IN PAGE TABLE\n");
                 // frame does not exist in page table
                 // grab from backing store
                 // bring to physical memory and add to page table
-                // printf("backing store: %s\n", backingStore[laddr.bf.pageno*8]);
-                char* page = "";
-                printf("%x", backingStore[laddr.bf.pageno]);
-                for (int i = laddr.bf.pageno; i < laddr.bf.pageno + laddr.bf.offset; ++i){
-                    printf("%x", backingStore[i]);
-                    strcat(page, &backingStore[i]);
+
+                char page[pageSize];
+                for (int i = 0; i < pageSize; ++i){
+                    page[i] = backingStore[i + (laddr.bf.pageno*pageSize)];
                 }
-                printf("Page Contents: %s\n", page);
-                physMem[laddr.bf.pageno] = &page;
+                if (sizeof(page) != 256){
+                    exit(EXIT_FAILURE);
+                }
+                // put page into physical memory
+                physMem[nextPhysMem] = page;
                 //update page table and tlb
-                frameno = laddr.bf.pageno;
+                frameno = nextPhysMem;
+                nextPhysMem++;
                 pt[laddr.bf.pageno]->frameno = frameno;
-                printf("FRAME IS CREATED IN PAGE TABLE\n");
             }else{
                 //frame already in page table
             }
 
             // put page into tlb
-            // check if tlb is full first
-            bool tlbfull = true;
-            for (int i = 0; i < numInTLB; ++i){
-                if (tlb[i] == NULL){
-                    tlbfull = false;
-                }
-            }
-            // create new tlb entry
             tlbent newtlbent;
             newtlbent.frameno = frameno;
             newtlbent.pageno = laddr.bf.pageno;
-            if(tlbfull){
+            TLBFull = (tlbstart == tlbend + 1) || (tlbstart == 0 && tlbend == TLBSize - 1) ? true : false;
+            if(TLBFull){ //chek if tlb full
                 // replace one from tlb
-                tlb[tlbstart] = &newtlbent; // free mem first?
-                tlbstart = (tlbstart + 1) % numInTLB;
+                tlb[tlbstart] = newtlbent; // free mem first?
+                tlbstart = (tlbstart + 1) % TLBSize;
+                tlbend = (tlbend + 1) % TLBSize;
             }else{
                 // add to tlb
-                tlb[tlbend] = &newtlbent;
-                tlbend = (tlbend + 1) % numInTLB; // not sure if necessary
+                if(tlbstart == -1){
+                    tlbstart = 0;
+                }
+                tlbend = (tlbend + 1) % TLBSize;
+                tlb[tlbend] = newtlbent;
             }
-            printf("add to tlb complete\n");
-
-            // create output
-            printf("Logical Address: %d\n", addr);
-            printf("Logical Address as Page Number and offset: %d + %d\n", laddr.bf.pageno, laddr.bf.offset);
-            printf("Physical Address as Frame Number and offset: %d + %d\n", frameno, offset);
-            printf("Signed value at Physical Address: %c\n", (*(physMem[frameno]))[offset]);
+        }else{
+            pageHits++;
         }
+
+        // create output
+        printf("Logical Address: %d\n", addr);
+        printf("Logical Address as Page Number and offset: %d + %d\n", laddr.bf.pageno, offset);
+        printf("Physical Address as Frame Number and offset: %d + %d\n", frameno, offset);
+        printf("Hex value at Physical Address: %x\n", (char)(physMem[frameno][offset]));
+        printf("\n");
     }
+
     printf("Statistics:\n");
     printf("Total Page Hits: %d\n",pageHits);
     printf("Total Page Misses: %d\n",pageMisses);
+    printf("Row Count: %d\n",rowCount);
+
+    printf("Page Hits Percentage: %f%%\n",(float)pageHits/rowCount * 100);
+    printf("Page Misses Percentage: %f%%\n",(float)pageMisses/rowCount * 100);
 
 
     fclose(fp);
